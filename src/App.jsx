02e38b6e3bec27bfd11820ecdd8732edd0729066
Harun Bappi <harunbappi1234@gmail.com>
2026-05-28 11:54:46 +0600

    services section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Sections/Hero';
 import About from './Components/Sections/About';
 import Skills from './Components/Sections/Skills';
 import Projects from './Components/Sections/Projects';
+import Services from './Components/Sections/Services';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <About></About>
           <Skills></Skills>
           <Projects></Projects>
+          <Services></Services>
         </main>
     </div>
   );
